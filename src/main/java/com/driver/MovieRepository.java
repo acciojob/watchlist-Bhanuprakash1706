ad3ccafa622ad10ebffc000a76bc828caebd0d02
[Bhanyprakash1706] <[mbhanuprakash1052000@gmail.com]>
2022-11-29 15:18:16 +0530

    first try

---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,96 @@
+package com.driver;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import javax.websocket.OnClose;
+import java.security.KeyPair;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Objects;
+
+@Repository
+@Component
+public class MovieRepository {
+   // HashMap<Movie,Director> ListOfPair=new HashMap<>();
+    HashMap<String,Movie> ListOfMoviesInDB=new HashMap<>();
+    HashMap<Director,LinkedList<Movie>> ListOfDirectorsInDB=new HashMap<>();
+    //Adding movie to DB
+    public void addMovieToDB(Movie movie){
+        ListOfMoviesInDB.put(movie.getName(), movie);
+    }
+    //Adding director to DB
+    public void addDirectorToDB(Director director){
+        LinkedList<Movie> movieListOfDirectors=new LinkedList<>();
+        ListOfDirectorsInDB.put(director,movieListOfDirectors);
+    }
+    //Pair movie and director
+    public void addMovieDirectorPairToDB(String movieName,String directorName){
+        for(Director director:ListOfDirectorsInDB.keySet()){
+            if(Objects.equals(director.getName(), directorName)){
+                LinkedList<Movie> temp;
+                temp=ListOfDirectorsInDB.get(director);
+                temp.add(ListOfMoviesInDB.get(movieName));
+                ListOfDirectorsInDB.put(director,temp);
+                break;
+            }
+        }
+
+    }
+
+    public Movie getMovieByNameFromDB(String name) {
+        return ListOfMoviesInDB.get(name);
+    }
+    //not working
+    public Director getDirectorByNameFromDB(String name) {
+        for(Director director:ListOfDirectorsInDB.keySet()){
+            if(Objects.equals(director.getName(), name)){
+                return director;
+            }
+        }
+        return null;
+    }
+
+    public LinkedList<Movie> findAllMoviesFromDB() {
+        LinkedList<Movie> listOfMovies=new LinkedList<>();
+        for(Movie movie:ListOfMoviesInDB.values()){
+            listOfMovies.add(movie);
+        }
+        return listOfMovies;
+    }
+
+    public LinkedList<Movie> getMoviesByDirectorNameFromDB(String dName) {
+    for(Director director:ListOfDirectorsInDB.keySet()){
+        if(Objects.equals(director.getName(),dName)){
+            return ListOfDirectorsInDB.get(director);
+        }
+    }
+    return null;
+    }
+
+    public void deleteDirectorByNameFromDB(String dName) {
+        for(Director director:ListOfDirectorsInDB.keySet()){
+            if(Objects.equals(director.getName(),dName)){
+                LinkedList<Movie> temp;
+                temp=ListOfDirectorsInDB.get(director);
+                for(Movie movie:temp){
+                    ListOfMoviesInDB.remove(movie.getName());
+                }
+                ListOfDirectorsInDB.remove(director);
+                break;
+            }
+        }
+    }
+
+    public void deleteAllDirectorsFromDB() {
+        for(Director director:ListOfDirectorsInDB.keySet()){
+            LinkedList<Movie> temp;
+            temp=ListOfDirectorsInDB.get(director);
+            for(Movie movie:temp){
+                ListOfMoviesInDB.remove(movie.getName());
+            }
+        }
+        ListOfDirectorsInDB.clear();
+    }
+    //
+}
---
+++ src/main/java/com/driver/MovieService.java
@@ -0,0 +1,52 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.LinkedList;
+
+@Service
+@Component
+public class MovieService {
+    @Autowired
+    MovieRepository movieRepository;
+    public void addAllMovies(Movie movie){
+        movieRepository.addMovieToDB(movie);
+    }
+    public void addAllDirectors(Director director){
+        movieRepository.addDirectorToDB(director);
+    }
+
+    public void addAllMovieDirectorPair(String mName, String dName) {
+        movieRepository.addMovieDirectorPairToDB(mName,dName);
+    }
+
+    public Movie getAllMoviesByName(String name) {
+        return movieRepository.getMovieByNameFromDB(name);
+    }
+
+    public Director getAllDirectorsByName(String name) {
+        return movieRepository.getDirectorByNameFromDB(name);
+    }
+
+    public LinkedList<Movie> findAllMovies() {
+        return movieRepository.findAllMoviesFromDB();
+    }
+
+    public LinkedList<Movie> getAllMoviesByDirectorName(String dName) {
+    return movieRepository.getMoviesByDirectorNameFromDB(dName);
+    }
+
+    public void deleteAllDirectorsByName(String dName) {
+        movieRepository.deleteDirectorByNameFromDB(dName);
+    }
+
+    public void deleteAllDirectors() {
+        movieRepository.deleteAllDirectorsFromDB();
+    }
+}
